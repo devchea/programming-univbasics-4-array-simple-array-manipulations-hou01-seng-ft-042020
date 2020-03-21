81f9d7f1341f91ba73348dea4b556bf4e252519a
Richard Chea <rchea817@gmail.com>
2020-03-21 03:49:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array,arrays)
   array.concat(arrays)
 end
 
-def using_insert(array)
-  array.insert(3,())
+def using_insert(array, element)
+  array.insert(3,)
 end
 
 def using_flatten(array)
